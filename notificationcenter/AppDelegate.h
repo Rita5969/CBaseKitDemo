6f972b7fd2c086f0fd9f156c238d1ec13c6b7af0
Rita <chegaar5969@foxmail.com>
2014-08-14 19:17:51 +0800

    Initial Commit

---
+++ notificationcenter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  notificationcenter
+//
+//  Created by Rita on 14-8-14.
+//  Copyright (c) 2014年 Rita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
